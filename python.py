78efbbf3dbca3c0a63e034c2f4d0954c6f5cef3a
ubeydullahbilgin <ubeydullahbilgin0@gmail.com>
2023-08-09 01:21:24 +0300

    08/08/2023, Tuesday

--- python.py
+++ python.py
@@ -1,45 +1,83 @@
-# Division
-print(5 / 8)
+print("Hello World!")
 
-# Addition
-print(7 + 10)
+# to give information about your code
+# there is comment lines in Python
 
-# Subtraction
-print(5 - 5)
+day_of_week = 5
+type(day_of_week)  # integer
 
-# Multiplication
-print(3 * 5)
+pi = 3.14
+type(pi)  # float
 
-# Creating a variable
-savings = 100
+bmi_read = "body mass index"
+type(bmi_read)  # string
 
-print(savings)
+z = True
+type(z)  # boolean
 
-# Calculations with variables
-monthly_savings = 10
-num_months = 4
+str(day_of_week)  # conversion to string
+float(day_of_week)  # conversion to float
+int(z)  # conversion to integer
+bool(0)  # conversion to boolean
 
-new_savings = monthly_savings * num_months
-total_savings = new_savings + savings
+# different types of list
+fam_heights = [1.65, 1.80, 1.75, 1.64, 1.68]
 
-print(total_savings)
+fam_random = ["Ruveyda", 1.80, True, 25]
 
-# integer
-total = 1
+members_heights = [["Ruveyda", 1.68],
+                   ["Ubeydullah", 1.80],
+                   ["Humeyra", 1.65],
+                   ["Fatma", 1.64],
+                   ["Mustafa", 1.75]]
 
-# float
-half = 0.5
+# index of list, slicing and dicing
+print(fam_heights[2])  # index
+print(fam_heights[-3])
 
-# string
-intro = "Hello! How are you?"
+print(fam_heights[0:3])  # slicing
+print(fam_heights[:3])
+print(fam_heights[-4:])
 
-# Boolean
-is_good = True
+fam_heights[0] = 1.76  # replacing list elements
 
-# Type of variables
-print(type(is_good))
+fam_heights += [1.60]  # extending a list
 
-str(variable_name) # Type conversion to string
-int(variable_name) # Type conversion to integer
-float(variable_name) # Type conversion to float
-bool(variable_name) # Type conversion to boolean
+del(fam_heights[-1])  # deleting an element from a list
+
+# important about memory
+list1 = [1, 2, 3, 4]
+list2 = list1
+
+"""
+if we make an assignment like the above every change
+in any list effect the other one. If we want to have
+a list just like the other one but independent one
+we should do it like the below.
+"""
+
+list3 = list(list1)
+list4 = list1[:]
+
+# some built-in functions
+len(list1)  # gives the num of elements in the list or string
+help(print)  # gives information about the structure of any built-in function
+
+list5 = sorted(list1)  # a copy of the list in ascending order
+list6 = sorted(list1, reverse=True)  # a copy of the list in ascending order
+
+# string methods
+bmi_read.upper()  # capitalizes letters
+bmi_read.count('b')  # counts the number of 'b'
+
+# list methods
+list1.index("2")  # finds the index of element
+list1.count(5)
+list1.append(3)  # adds the element to end of the list
+list1.remove(1)  # removes the element
+list1.reverse()  # edits the list from last to first
+
+# importing a package
+import numpy as np
+
+from numpy import array  # selective import
