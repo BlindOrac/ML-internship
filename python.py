e342e6329e10eefbda8e5f830129b6f2d9dfda10
ubeydullahbilgin <ubeydullahbilgin0@gmail.com>
2023-08-08 17:21:45 +0300

    08/08/2023, Tuesday

---
+++ python.py
@@ -0,0 +1,45 @@
+# Division
+print(5 / 8)
+
+# Addition
+print(7 + 10)
+
+# Subtraction
+print(5 - 5)
+
+# Multiplication
+print(3 * 5)
+
+# Creating a variable
+savings = 100
+
+print(savings)
+
+# Calculations with variables
+monthly_savings = 10
+num_months = 4
+
+new_savings = monthly_savings * num_months
+total_savings = new_savings + savings
+
+print(total_savings)
+
+# integer
+total = 1
+
+# float
+half = 0.5
+
+# string
+intro = "Hello! How are you?"
+
+# Boolean
+is_good = True
+
+# Type of variables
+print(type(is_good))
+
+str(variable_name) # Type conversion to string
+int(variable_name) # Type conversion to integer
+float(variable_name) # Type conversion to float
+bool(variable_name) # Type conversion to boolean
